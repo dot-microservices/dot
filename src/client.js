426054b58da81dcdf801b772c0e6b78af5b26ec0
umu+ <aydinumut82@gmail.com>
2018-11-09 08:35:07 +0300

    methods which start with _ is private

--- src/client.js
+++ src/client.js
@@ -154,8 +154,12 @@ class Client extends Base {
         const delimiter = this.options.delimiter;
         const delay = this.options.delay;
         const timeout = this.options.timeout, useTimeout = is.number(timeout) && timeout > 0;
-        const service = path.split(is.string(delimiter) && is.not.empty(delimiter) ? delimiter : '.').shift();
-        const socket = this._getSocket(service);
+        const service = path.split(is.string(delimiter) && is.not.empty(delimiter) ? delimiter : '.');
+        if (service < 2) return cb(new Error('MISSING_METHOD'));
+        else if (!service[1].trim().length || service[1].charAt(0) === '_')
+            return cb(new Error('INVALID_METHOD'));
+
+        const socket = this._getSocket(service[0]);
         if (socket) {
             let t_o = null;
             try {

8753ab4529b498c05caf92ef27f1c230aa5fcd60
umu+ <aydinumut82@gmail.com>
2019-09-27 08:40:08 +0300

    tape implementation replaced with jest

--- tests/client.test.js
+++ tests/client.test.js
@@ -4,33 +4,22 @@
 
 const buildPath = require('path').join;
 const Client = require('../src/client');
-const is = require('is_js');
 const { spawn } = require('child_process');
-const test = require('tape').test;
 
 const client = new Client({ secret: 'test', group: 'test', timeout: 4000 });
 
 const server = buildPath(__dirname, '/server.js');
 const child = spawn(server);
 
-setTimeout(() => {
-    test('Check basic socket communication', function (assert) {
-        const started = setTimeout(() => {
-            assert.fail('service time out');
+test('Check basic socket communication', done => {
+    setTimeout(() => {
+        const payload = Math.random();
+        client.send('t.echo', payload, r => {
             child.stdin.pause();
             child.kill();
             client.shutdown(() => client.disconnect());
-            assert.end();
-        }, 5000);
-
-        const payload = Math.random();
-        client.send('t.echo', payload, response => {
-            clearTimeout(started);
-            if (is.error(response)) assert.fail(response.message);
-            else if (response === payload) assert.ok(response, `${response} received successfully!`);
-            else assert.fail('invalid response');
-            client.shutdown(() => client.disconnect());
-            assert.end();
+            expect(r).toBe(payload);
+            done();
         });
-    });
-}, 2000);
+    }, 3000);
+});
